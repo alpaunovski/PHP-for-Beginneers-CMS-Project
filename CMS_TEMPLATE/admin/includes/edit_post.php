b60e2f54940a044991ac210e051a896dd1d2cc47
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-04-02 06:58:12 +0300

    minor fixes

--- CMS_TEMPLATE/admin/includes/edit_post.php
+++ CMS_TEMPLATE/admin/includes/edit_post.php
@@ -23,14 +23,39 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)){
 }
 
 if(isset($_POST["update_post"])){
-    $post_author=$_POST["post_author"];
-    $post_title=$_POST["post_title"];
-    $post_category_id=["post_category"];
-    $post_status=$_POST["post_status"];
-    $post_image=$_FILES["image"]["name"];
-    $post_image_temp=$_FILES["image"]["tmp_name"];
-    $post_content=$_POST["post_content"];
-    $post_tags=$_POST["post_tags"];
+    $post_author= $_POST["author"];
+    $post_title= $_POST["title"];
+    $post_category_id= $_POST["post_category"];
+    $post_status= $_POST["post_status"];
+    $post_image= $_FILES["image"]["name"];
+    $post_image_temp= $_FILES["image"]["tmp_name"];
+    $post_content= $_POST["post_content"];
+    $post_tags= $_POST["post_tags"];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if(empty($post_image)){
+        $query="SELECT * FROM posts WHERE post_id=$the_post_id ";
+        $select_image = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($select_image)){
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .="post_title = '{$post_title}', ";
+    $query .="post_category_id = '{$post_category_id}', ";
+    $query .="post_date = now(), ";
+    $query .="post_author = '{$post_author}', ";
+    $query .="post_status = '{$post_status}', ";
+    $query .="post_tags = '{$post_tags}', ";
+    $query .="post_content = '{$post_content}', ";
+    $query .="post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id}";
+
+    $update_post=mysqli_query($connection, $query);
+
 }
 
 ?>
@@ -82,6 +107,7 @@ if(isset($_POST["update_post"])){
 <div class="form-group">
     <label for="image">Post Image</label>
     <img width="100px" src="../images/<?php echo $post_image ?>" >
+    <input type="file" name="image">
 </div>
 
 <div class="form-group">
