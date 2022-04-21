7eba3931720bc93937c5b7cec7e025b6c0fa0b1d
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-04-21 08:22:49 +0300

    Updating User Data

--- CMS_TEMPLATE/admin/profile.php
+++ CMS_TEMPLATE/admin/profile.php
@@ -27,6 +27,41 @@ if(isset($_SESSION['username'])){
 
 ?>
 
+<?php 
+
+
+if(isset($_POST['edit_user'])){
+    // $user_id = $_POST["user_id"];
+    $user_firstname = $_POST["user_firstname"];
+    $user_lastname = $_POST["user_lastname"];
+    $user_role = $_POST["user_role"];
+
+    // $post_image = $_FILES["image"]["name"];
+    // $post_image_temp = $_FILES["image"]["tmp_name"];
+
+    $username = $_POST["username"];
+    $user_email = $_POST["user_email"];
+    $user_password = $_POST["user_password"];
+    // $post_date = date("d-m-y");
+    // $post_comment_count = 4;
+
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "UPDATE users SET ";
+    $query .="user_firstname = '{$user_firstname}', ";
+    $query .="user_lastname = '{$user_lastname}', ";
+    $query .="user_role = '{$user_role}', ";
+    $query .="username = '{$username}', ";
+    $query .="user_email = '{$user_email}', ";
+    $query .="user_password = '{$user_password}' ";
+    $query .= "WHERE username = '{$username}' ";
+
+    $edit_user_query=mysqli_query($connection, $query);
+    confirm($edit_user_query);
+}
+
+?>
+
     <div id="wrapper">
 
         <!-- Navigation -->
