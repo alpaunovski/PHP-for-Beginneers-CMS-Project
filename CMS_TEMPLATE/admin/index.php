ba242c61feaf59a276870c63e6ecddaa11e3b931
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-10 08:37:12 +0300

    223. Displaying Users Online :)

--- CMS_TEMPLATE/admin/index.php
+++ CMS_TEMPLATE/admin/index.php
@@ -17,7 +17,13 @@
 
         if($count == NULL) {
             mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+        } else {
+            mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session' ");
         }
+
+        $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out' ");
+
+        $count_user = mysqli_num_rows($users_online_query);
 
 
 
@@ -39,6 +45,11 @@
                             <small><?php echo $_SESSION['firstname'] ?></small>
 
 
+                        </h1>
+
+                        <h1>
+
+                        <?php echo $count_user; ?>
                         </h1>
                         
                     </div>
