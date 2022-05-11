c5fa3b1ad9cc48f754a80deedf42f70e2f50ff12
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-11 08:47:00 +0300

    233. Comment count and amount display

--- CMS_TEMPLATE/admin/includes/view_all_posts.php
+++ CMS_TEMPLATE/admin/includes/view_all_posts.php
@@ -142,7 +142,14 @@
                                         
                                         echo "<td><img width='100px' src='../images/$post_image' ></td>";
                                         echo "<td>{$post_tags}</td>";
-                                        echo "<td>{$post_comment_count}</td>";
+
+                                        $query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
+                                        $send_comment_query = mysqli_query($connection, $query);
+                                        $count_comments = mysqli_num_rows($send_comment_query);
+
+                                        echo "<td>{$count_comments}</td>";
+
+
                                         echo "<td>{$post_date}</td>";
                                         echo "<td><a href='../post.php?p_id=$post_id'>View Post</a></td>";
                                         echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
