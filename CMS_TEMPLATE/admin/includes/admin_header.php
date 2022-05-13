0d151db211cb4de8086fb8f66a561f4e89be5cfb
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-13 11:19:24 +0300

    255. Creating the markup

--- CMS_TEMPLATE/admin/includes/admin_header.php
+++ CMS_TEMPLATE/admin/includes/admin_header.php
@@ -51,6 +51,7 @@ if(!isset($_SESSION['user_role'])){
 
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="js/jquery.js"></script>
     
 
 </head>
--- CMS_TEMPLATE/admin/includes/view_all_posts.php
+++ CMS_TEMPLATE/admin/includes/view_all_posts.php
@@ -1,4 +1,5 @@
                     <?php 
+                    include("delete_modal.php");
 
                     if(isset($_POST['checkBoxArray'])){
                         foreach($_POST['checkBoxArray'] as $postValueId ){
@@ -168,7 +169,8 @@
                                         echo "<td>{$post_date}</td>";
                                         echo "<td><a href='../post.php?p_id=$post_id'>View Post</a></td>";
                                         echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
-                                        echo "<td><a onClick=\" javascript: return confirm('Are you sure you want to delete') \" href='posts.php?delete=$post_id'>Delete</a></td>";
+                                        echo "<td><a rel='$post_id' href='' class='delete_link'>Delete</a></td>";
+                                        // echo "<td><a onClick=\" javascript: return confirm('Are you sure you want to delete') \" href='posts.php?delete=$post_id'>Delete</a></td>";
                                         echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                                         
 
@@ -206,3 +208,13 @@
                         }
                         
                         ?>
+
+                        <script>
+                            $(document).ready(function(){
+                                $(".delete_link").on('click', function(){
+                                    var id = $(this).attr("rel");
+
+                                    alert(id);
+                                });
+                            });
+                        </script>
