8a3b69700b72dad34949594531f4ee24e184ca6b
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-13 11:57:23 +0300

    259. No categories feature

--- CMS_TEMPLATE/category.php
+++ CMS_TEMPLATE/category.php
@@ -18,10 +18,17 @@
 
             if(isset($_GET["category"])){
                 $post_category_id = escape($_GET["category"]);
-            }
-
-                $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id";
+            
+                $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id AND post_status = 'published' ";
                 $select_all_posts_query = mysqli_query($connection, $query);
+
+                if(mysqli_num_rows($select_all_posts_query) < 1){
+
+                    echo "<h1 class='text-center' >No categories available</h1>";
+
+                } else {
+
+                
 
                 while ($row = mysqli_fetch_assoc($select_all_posts_query)){
                     $post_id = $row["post_id"];
@@ -52,7 +59,11 @@
 
                 <hr>
 
-            <?php } ?>
+            <?php }} }else {
+
+                header("Location: index.php");
+             }
+                ?>
 
 
 
