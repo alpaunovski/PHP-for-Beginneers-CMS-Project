75e179b283607f15f5d404e394e7d3fc2195e93c
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-17 11:41:17 +0300

    270. Refactoring our view all posts by JOINING TABLES part 2

--- CMS_TEMPLATE/admin/includes/view_all_posts.php
+++ CMS_TEMPLATE/admin/includes/view_all_posts.php
@@ -105,9 +105,9 @@
                                 <?php 
                                     // $query = "SELECT * FROM posts ";
 
-                                    $query = "SELECT posts.post_id, posts.post.author, posts.post_user, posts.post_title, posts.post_category_id, posts.post_status, posts.post_image, ";
+                                    $query = "SELECT posts.post_id, posts.post_author, posts.post_user, posts.post_category_id, posts.post_status, posts.post_title, posts.post_image, ";
                                     $query .= "posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count, categories.cat_id, categories.cat_title FROM posts ";
-                                    $query .= "LEFT JOIN categories ON posts.post_category_id = categories.cat_id";
+                                    $query .= "LEFT JOIN categories ON posts.post_category_id = categories.cat_id ORDER BY posts.post_id DESC";
                                     $select_posts = mysqli_query($connection, $query);
 
                                     while ($row = mysqli_fetch_assoc($select_posts)){
@@ -123,6 +123,8 @@
                                         $post_comment_count = $row["post_comment_count"];
                                         $post_date = $row["post_date"];
                                         $post_views_count = $row["post_views_count"];
+                                        $cat_title = $row['cat_title'];
+                                        $cat_id = $row['cat_id'];
 
                                         echo "<tr>";
 
@@ -145,15 +147,15 @@
 
                                         echo "<td>{$post_title}</td>";
 
-                                        $query = "SELECT * FROM categories WHERE cat_id = {$post_category}";
-                                        $select_categories_id = mysqli_query($connection, $query);
+                                        // $query = "SELECT * FROM categories WHERE cat_id = {$post_category}";
+                                        // $select_categories_id = mysqli_query($connection, $query);
                             
-                                        while ($row = mysqli_fetch_assoc($select_categories_id)){
-                                            $cat_id = $row["cat_id"];
-                                            $cat_title = $row["cat_title"];
+                                        // while ($row = mysqli_fetch_assoc($select_categories_id)){
+                                        //     $cat_id = $row["cat_id"];
+                                        //     $cat_title = $row["cat_title"];
 
                                         echo "<td>{$cat_title}</td>";
-                                        }
+                                        // }
 
 
                                         echo "<td>{$post_status}</td>";
