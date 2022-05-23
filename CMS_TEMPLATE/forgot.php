591f919ec91109072bcc774d57ff3fa6b30f71ab
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-23 08:58:55 +0300

    291. Updating database with token values

--- CMS_TEMPLATE/forgot.php
+++ CMS_TEMPLATE/forgot.php
@@ -3,11 +3,36 @@
 
 <?php 
 
-if(!ifItIsMethod('GET') || !$_GET['forgot']){
+if(!ifItIsMethod('GET') && !isset($_GET['forgot'])){
 
     redirect('index.php');
 }
 
+
+if(!ifItIsMethod('POST')){
+
+    if(isset($_POST['email'])){
+        $email = $_POST['email'];
+
+        $length = 50;
+
+        $token = bin2hex(openssl_random_pseudo_bytes($length));
+
+        if(email_exists($email)){
+    
+            if($stmt = mysqli_prepare($connection, "UPDATE users SET token='{$token} WHERE user_email = ?'")){
+                mysqli_stmt_bind_param($stmt, "s". $email);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_close($stmt);
+            } else {
+                echo "WRONG";
+            }
+
+
+    
+        }
+    }
+}
 
 ?>
 
