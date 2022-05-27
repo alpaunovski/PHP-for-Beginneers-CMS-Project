41ab9c3f5ed27b47da90d08cb3983311cea662b2
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-05-27 09:31:30 +0300

    306. Update search feature to work with the forgot password system code

--- CMS_TEMPLATE/includes/sidebar.php
+++ CMS_TEMPLATE/includes/sidebar.php
@@ -1,12 +1,16 @@
 <?php 
 
 if(ifItIsMethod('POST')){
+    if(isset($_POST['login'])){
+
+    
     if(isset($_POST['username']) && isset($_POST['password'])) {
         login_user($_POST['username'], $_POST['password']);
 
     } else {
         redirect('/cms/index.php');
     }
+}
 }
 
 
